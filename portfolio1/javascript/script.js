5e4856c0274c199d3142588160880625f1ea55a8
Marc-Franken <70567034+Marc-Franken@users.noreply.github.com>
2020-11-12 15:37:38 +0100

    commit

--- portfolio1/javascript/script.js
+++ portfolio1/javascript/script.js
@@ -12,3 +12,19 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+$(function(){
+  var documentEl = $(document),
+    fadeElem = $('.fade-scroll');
+
+    documentEl.on('scroll', function() {
+      var currScrollPos = documentEl.scrollTop();
+
+        fadeElem.each(function(){
+          var $this = $(this),
+          elemOffsetTop = $this.offset().top;
+          if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 -(currScrollPos-elemOffsetTop)/400);
+
+        });
+    });
+});
